1c507f39495e3a012190f635f2de6902bb81e06b
RolyGaming <xgabriel37x@gmail.com>
2024-10-09 22:22:46 -0300

    Update Connection.php

--- addons/amxmodx/stats/www/class/Connection.php
+++ addons/amxmodx/stats/www/class/Connection.php
@@ -17,9 +17,9 @@ class Connection
             {
                 self::$pdo = new \PDO
                 (
-                    "mysql:host=45.235.98.50;port=3306;dbname=alejandrol_pubtopsito",
-                    'alejandrol_511',
-                    'R5!CkjY576JW',
+                    "mysql:host=localhost;port=3306;dbname=Database",
+                    'UserDB',
+                    'PassDB',
                     [
                         PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
                         PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_ASSOC,
